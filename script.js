bbc08e9d2ccbad0246327d3037c03221e413f621
Adekunle3060 <adekunlesunkanmi59@gmail.com>
2025-07-10 15:46:26 +0100

    i added a new to-do list app

---
+++ script.js
@@ -0,0 +1,52 @@
+
+     alert("connected");
+
+// $("li").click(function () {
+//     $(this).css("color","red" )
+//     $(this).css("text-decoration", "line-through");
+// });
+
+
+$("ul").on("click", "li", function () {
+    $(this).toggleClass("completed")
+});
+
+
+$("ul").on("click", "span",function () {
+    $(this).parent().fadeOut(5000, function() {
+        $(this).remove()
+    });
+});
+
+$("input").keypress(function(event) {
+    if (event.which === 13) {
+        let todoText = $(this).val();
+        $(this).val("")
+        
+    $("ul").append("<li> <span> <i class='fa fa-trash'></i> </span>" + todoText + "</li");
+}
+});
+
+
+//     if ($(this).css("color")==="rgb(128, 128, 128)") {
+//         $(this).css("color", "black")
+//         $(this).css("text-decoration", "none")
+
+//     }else {
+//     $(this).css("color", "grey")
+//     $(this).css("text-decoration", "line-through")
+
+//     }
+// });
+// // $("li").click (function () {
+// //  $(this).css({
+// //     color: "grey",
+// //     textDecoration: "line-through"
+    
+// //  });
+// });
+
+// if (this.color === "grey") {
+
+// console.log($(this).css("color"));
+
